038dc7c42765a9d51bc0711328e5625d80637812
Alvin Lin <alin@istuary.com>
2013-12-16 19:21:39 -0800

    remove unused import

--- src/test/java/io/gatling/mojo/GatlingMojoUtilsTest.java
+++ src/test/java/io/gatling/mojo/GatlingMojoUtilsTest.java
@@ -19,7 +19,6 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 
-import org.apache.maven.project.MavenProject;
 import org.junit.Test;
 
 import scala_maven_executions.MainHelper;
